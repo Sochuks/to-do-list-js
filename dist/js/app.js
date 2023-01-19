d2f50d0f5d09dce6a20852bb98b746e1a12ee8ff
Sochuks <moseschukwumakajang@gmail.com>
2023-01-19 13:24:41 +0100

    Modified app on click add new item

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,9 +5,22 @@ let task = document.querySelector('.task');
 
 // disable list form btn 
 input.addEventListener('keyup', () => {
-    if (input.value.trim !== 0 ){
+    if (input.value.trim() !== 0 ){
         addBtn.classList.add('active');
 } else{
     addBtn.classList.remove('active');
 }
+})
+
+// add items to task
+addBtn.addEventListener('click', ()  =>{
+    if (input.value.trim() !==0){
+
+        // create new task
+        newItem = document.createElement('div');
+        newItem.classList.add('item');
+        newItem.innerHTML = `
+        
+        `
+    }
 })
